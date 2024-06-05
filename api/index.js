4a0e21d39595003e3dca3f5de94fa81afd900b0e
Armandilo <muhammadarmand@student.usm.my>
2024-06-06 04:49:01 +0800

    module name

--- api/index.js
+++ api/index.js
@@ -16,6 +16,11 @@ import userreviewRoute from "./routes/userreview.route.js";
 import barterRoute from "./routes/barter.route.js";
 import { verifyToken } from "./middleware/jwt.js";
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 dotenv.config();
