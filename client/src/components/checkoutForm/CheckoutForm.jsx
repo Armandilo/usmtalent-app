c4448914411f90311fafab06186d20e86893ab28
Armandilo <muhammadarmand@student.usm.my>
2024-06-06 02:56:22 +0800

    payment

--- client/src/components/checkoutForm/CheckoutForm.jsx
+++ client/src/components/checkoutForm/CheckoutForm.jsx
@@ -71,7 +71,7 @@ const CheckoutForm = () => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:5173/success",
+        return_url: "https://usmtalent-app.vercel.app/success",
       },
     });
 
